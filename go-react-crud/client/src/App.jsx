f9292ec03efb604585ffa9d072696b517b3c8328
RaulCudris <raulcudriz@hotmail.com>
2023-11-25 18:38:25 -0500

    Actualizacion del Proyecto en react form

--- go-react-crud/client/src/App.jsx
+++ go-react-crud/client/src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react"
 
 function App() {
-
-  const handleSubmit =() =>{
-
+  const [name , setName] = useState('')
+  const handleSubmit =  async (e) =>{
+    e.preventDefault()
+    const response = await fetch(import.meta.env.VITE_API+'/users',{
+      method: 'POST',
+      body: JSON.stringify({name})
+    })
+    const data = await response.json()
+    console.log(data)
   }
 
   return (
     <div>
-      <form onSubmit={()=> handleSubmit()}>
-        <input type="name" placeholder="Coloca tu nombre"/>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="name" 
+        placeholder="Coloca tu nombre"
+        onChange={(e)=> setName(e.target.value)}/>
         <button>Guardar</button>
       </form>
     </div>   
